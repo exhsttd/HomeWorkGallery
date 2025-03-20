4277635249862e47868aee05914c5d9b78ceb932
exhsttd <rlisutkina@gmail.com>
2025-03-20 19:45:58 +0300

    Add files via upload

---
+++ HomeWorkGallery/HomeWorkGallery/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace HomeWorkGallery
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxImages = new System.Windows.Forms.ComboBox();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxImages
+            // 
+            this.comboBoxImages.FormattingEnabled = true;
+            this.comboBoxImages.Location = new System.Drawing.Point(61, 160);
+            this.comboBoxImages.Name = "comboBoxImages";
+            this.comboBoxImages.Size = new System.Drawing.Size(232, 24);
+            this.comboBoxImages.TabIndex = 0;
+            this.comboBoxImages.Text = "Выберите изображение";
+            this.comboBoxImages.SelectedIndexChanged += new System.EventHandler(this.comboBoxImages_SelectedIndexChanged);
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(61, 120);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(232, 22);
+            this.textBoxTitle.TabIndex = 1;
+            this.textBoxTitle.TextChanged += new System.EventHandler(this.textBoxTitle_TextChanged);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.pictureBox.Location = new System.Drawing.Point(330, 45);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(427, 328);
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(637, 394);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(120, 28);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Font = new System.Drawing.Font("Adobe Arabic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnLoad.ForeColor = System.Drawing.Color.Black;
+            this.btnLoad.Location = new System.Drawing.Point(61, 202);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(120, 28);
+            this.btnLoad.TabIndex = 4;
+            this.btnLoad.Text = "Загрузить";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(58, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Список загруженных изображений";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(468, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "место для изображения";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.comboBoxImages);
+            this.Name = "Form1";
+            this.Text = "Галерея";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxImages;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ HomeWorkGallery/HomeWorkGallery/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace HomeWorkGallery
+{
+   
+
+    public partial class Form1 : Form
+    {
+        public class ImageItem
+        {
+            public string Path { get; set; }
+            public string Title { get; set; }
+
+            public override string ToString()
+            {
+                return Title; 
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private List<ImageItem> imageItems = new List<ImageItem>();
+        private void btnLoad_Click(object input, EventArgs a)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Text files (*.txt)|*.txt";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string[] lines = File.ReadAllLines(openFileDialog.FileName);
+                    imageItems.Clear();
+                    comboBoxImages.Items.Clear();
+
+                    foreach (var line in lines)
+                    {
+                        var parts = line.Split(',');
+                        if (parts.Length == 2)
+                        {
+                            string path = parts[0].Trim();
+                            string title = parts[1].Trim();
+                            var imageItem = new ImageItem { Path = path, Title = title };
+                            imageItems.Add(imageItem);
+                            comboBoxImages.Items.Add(imageItem);
+                        }
+                    }
+                    MessageBox.Show("Данные загружены");
+                }
+            }
+        }
+
+        private void btnSave_Click(object input, EventArgs a)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Text files (*.txt)|*.txt";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamWriter writer = new StreamWriter(saveFileDialog.FileName))
+                    {
+                        foreach (var item in imageItems)
+                        {
+                            writer.WriteLine($"{item.Path},{item.Title}");
+                        }
+                    }
+                    MessageBox.Show("Данные сохранены");
+                }
+            }
+        }
+
+        private void comboBoxImages_SelectedIndexChanged(object input, EventArgs a)
+        {
+            if (comboBoxImages.SelectedItem is ImageItem selectedItem)
+            {
+                textBoxTitle.Text = selectedItem.Title;
+
+                if (File.Exists(selectedItem.Path))
+                {
+                    try
+                    {
+                        pictureBox.Image?.Dispose(); 
+                        pictureBox.Image = Image.FromFile(selectedItem.Path);
+                        label2.Visible = false;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Изображение не загрузилось: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Файл не существует: " + selectedItem.Path);
+                }
+            }
+        }
+
+        private void textBoxTitle_TextChanged(object input, EventArgs a)
+        {
+
+        }
+
+        private void label1_Click(object input, EventArgs a)
+        {
+
+        }
+
+        private void pictureBox_Click(object input, EventArgs a)
+        {
+
+        }
+    }
+}
